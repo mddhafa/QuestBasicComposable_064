4a6090faeb13282196ec2fcfa898f19a728b4625
mddhafa <mdmuhammaddhafa1234@gmail.com>
2024-10-17 18:46:02 +0700

    Membuat fungsi BasicCompose lalu menambahkan column agar content terisi ke bawah

--- app/src/main/java/com/example/basiccomposable/MainActivity.kt
+++ app/src/main/java/com/example/basiccomposable/MainActivity.kt
@@ -4,13 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.basiccomposable.ui.theme.BasicComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicComposableTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    BasicCompose(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -30,18 +38,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BasicComposableTheme {
-        Greeting("Android")
-    }
-}
+fun BasicCompose(
+    modifier: Modifier = Modifier
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    )
+
